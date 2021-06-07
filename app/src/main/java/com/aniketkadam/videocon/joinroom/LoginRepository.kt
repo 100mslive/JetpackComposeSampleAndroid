5bea28955bb7e4c17ca61f61161a6c0ea89e9da0
Aniket Kadam <anikadamg@gmail.com>
2021-06-07 16:01:36 +0530

    Created a repo for the room vm

---
+++ app/src/main/java/com/aniketkadam/videocon/joinroom/LoginRepository.kt
@@ -0,0 +1,25 @@
+package com.aniketkadam.videocon.joinroom
+
+import com.aniketkadam.videocon.tokenretriever.TokenRequest
+import com.aniketkadam.videocon.tokenretriever.TokenRequestApi
+import com.aniketkadam.videocon.tokenretriever.TokenResponse
+import io.reactivex.rxjava3.core.Observable
+import live.hms.video.sdk.HMSSDK
+import live.hms.video.sdk.HMSUpdateListener
+import live.hms.video.sdk.models.HMSConfig
+import javax.inject.Inject
+
+class LoginRepository @Inject constructor(
+    private val tokenRequestApi: TokenRequestApi,
+    private val hmssdk: HMSSDK
+) {
+
+    fun login(userName: String): Observable<TokenResponse> {
+        return tokenRequestApi.getToken(TokenRequest(userId = userName))
+    }
+
+    fun joinRoom(userName: String, authToken: String, updateListener : HMSUpdateListener) {
+            val config = HMSConfig(userName, authToken)
+            hmssdk.join(config,updateListener)
+    }
+}
