d6297232c629b1c74b89324e31ecb4e859a3eae9
Aniket Kadam <anikadamg@gmail.com>
2021-06-07 16:01:23 +0530

    Allowing injection of 100ms sdk

--- app/src/main/java/com/aniketkadam/videocon/videosdk/VideoModule.kt
+++ app/src/main/java/com/aniketkadam/videocon/videosdk/VideoModule.kt
@@ -3,15 +3,21 @@ package com.aniketkadam.videocon.videosdk
 import android.app.Application
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import live.hms.video.sdk.HMSSDK
 import live.hms.video.sdk.models.enums.HMSAnalyticsEventLevel
 import live.hms.video.utils.HMSLogger
+import javax.inject.Singleton
 
+@InstallIn(SingletonComponent::class)
 @Module
 object VideoModule {
 
+    @Singleton
     @Provides
     fun getVideoSdk(application: Application): HMSSDK {
+
         return HMSSDK
             .Builder(application)
             .setAnalyticEventLevel(HMSAnalyticsEventLevel.ERROR)
