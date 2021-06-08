3c2da8180f73632947c126b2956aea634ce60ccc
Aniket Kadam <anikadamg@gmail.com>
2021-06-08 18:47:51 +0530

    Rearranged the UI to be amenable

--- app/src/main/java/com/aniketkadam/videocon/MainActivity.kt
+++ app/src/main/java/com/aniketkadam/videocon/MainActivity.kt
@@ -14,25 +14,22 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            VideoConTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    VideoConTheme {
+        Surface(color = MaterialTheme.colors.background) {
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    VideoConTheme {
-        Greeting("Android")
-    }
+    MainScreen()
 }
