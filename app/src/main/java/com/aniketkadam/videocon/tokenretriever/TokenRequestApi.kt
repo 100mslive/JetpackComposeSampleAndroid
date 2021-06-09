25f1ced67c9b9fb78ed26e832cf70005ceeff0d3
Aniket Kadam <anikadamg@gmail.com>
2021-06-09 16:48:56 +0530

    Just using the entire token endpoint for calls

--- app/src/main/java/com/aniketkadam/videocon/tokenretriever/TokenRequestApi.kt
+++ app/src/main/java/com/aniketkadam/videocon/tokenretriever/TokenRequestApi.kt
@@ -1,10 +1,11 @@
 package com.aniketkadam.videocon.tokenretriever
 
+import com.aniketkadam.videocon.BuildConfig
 import io.reactivex.rxjava3.core.Observable
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface TokenRequestApi {
-    @POST("/")
+    @POST(BuildConfig.TOKEN_ENDPOINT)
     fun getToken(@Body tokenRequest: TokenRequest): Observable<TokenResponse>
 }
