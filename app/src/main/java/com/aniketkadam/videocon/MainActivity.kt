08c0d626eb858a157cd2e12dcf202fa4838433fc
Aniket Kadam <anikadamg@gmail.com>
2021-06-09 16:51:04 +0530

    Put together the UI for the app

--- app/src/main/java/com/aniketkadam/videocon/MainActivity.kt
+++ app/src/main/java/com/aniketkadam/videocon/MainActivity.kt
@@ -3,33 +3,62 @@ package com.aniketkadam.videocon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.LaunchedEffect
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.aniketkadam.videocon.joinroom.LoginState
+import com.aniketkadam.videocon.joinroom.RoomVm
+import com.aniketkadam.videocon.joinroom.VideosListComposable
+import com.aniketkadam.videocon.loading.LoadingScreen
+import com.aniketkadam.videocon.login.LoginScreen
 import com.aniketkadam.videocon.ui.theme.VideoConTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen()
+            VideoConTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    val navController = rememberNavController()
+
+                    val vm by viewModels<RoomVm>()
+
+                    NavHost(navController, startDestination = "login") {
+
+                        composable("login") {
+                            LoginScreen(vm::login)
+                        }
+
+                        composable("loading") {
+                            LoadingScreen()
+                        }
+
+                        composable("room") {
+                            VideosListComposable(vm.peers)
+                        }
+                    }
+
+                    LaunchedEffect(key1 = vm.loginState.value) {
+                        when (vm.loginState.value) {
+                            is LoginState.Error -> { // Just showing login again
+                                navController.navigate("login")
+                            }
+                            LoginState.LOADING -> navController.navigate("loading")
+                            LoginState.LoggedIn -> navController.navigate("room")
+                            LoginState.IDLE -> {
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
-}
-
-@Composable
-fun MainScreen() {
-    VideoConTheme {
-        Surface(color = MaterialTheme.colors.background) {
-
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MainScreen()
 }
