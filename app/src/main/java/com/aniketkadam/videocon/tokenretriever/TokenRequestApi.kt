0b4896bbec393e89a2e561368af6cd0149a8abc3
Aniket Kadam <anikadamg@gmail.com>
2021-06-09 17:16:14 +0530

    Corrected TokenRequest api url

--- app/src/main/java/com/aniketkadam/videocon/tokenretriever/TokenRequestApi.kt
+++ app/src/main/java/com/aniketkadam/videocon/tokenretriever/TokenRequestApi.kt
@@ -1,11 +1,10 @@
 package com.aniketkadam.videocon.tokenretriever
 
-import com.aniketkadam.videocon.BuildConfig
 import io.reactivex.rxjava3.core.Observable
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface TokenRequestApi {
-    @POST(BuildConfig.TOKEN_ENDPOINT)
+    @POST("api/token")
     fun getToken(@Body tokenRequest: TokenRequest): Observable<TokenResponse>
 }
