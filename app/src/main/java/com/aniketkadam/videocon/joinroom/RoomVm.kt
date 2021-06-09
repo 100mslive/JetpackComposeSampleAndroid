0ccb0e6661d23709a98cead606b735aa791cdfe9
Aniket Kadam <anikadamg@gmail.com>
2021-06-09 16:50:02 +0530

    Implement preliminary logic for rooms

--- app/src/main/java/com/aniketkadam/videocon/joinroom/RoomVm.kt
+++ app/src/main/java/com/aniketkadam/videocon/joinroom/RoomVm.kt
@@ -5,6 +5,9 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import live.hms.video.error.HMSException
 import live.hms.video.media.tracks.HMSTrack
 import live.hms.video.sdk.HMSUpdateListener
@@ -20,20 +23,36 @@ import javax.inject.Inject
 @HiltViewModel
 class RoomVm @Inject constructor(private val loginRepository: LoginRepository) : ViewModel() {
 
-    private val userName : MutableState<String> = mutableStateOf("")
-    private val _loginState : MutableState<LoginState> = mutableStateOf(LoginState.IDLE)
-    val loginState : State<LoginState> = _loginState
+    private val disposable = CompositeDisposable()
+    private val userName: MutableState<String> = mutableStateOf("")
+    private val _loginState: MutableState<LoginState> = mutableStateOf(LoginState.IDLE)
+    val loginState: State<LoginState> = _loginState
 
+    private val roomName: MutableState<String> = mutableStateOf("")
+    private val _peers: MutableState<List<HMSPeer>> = mutableStateOf(emptyList<HMSPeer>())
+    val peers: State<List<HMSPeer>> = _peers
 
-    private val roomName : MutableState<String> = mutableStateOf("")
-    private val _peers : MutableState<List<HMSPeer>> = mutableStateOf(emptyList<HMSPeer>())
-    val peers : State<List<HMSPeer>> = _peers
+    private val chatMessages: MutableState<List<HMSMessage>> = mutableStateOf(emptyList())
 
-    private val chatMessages : MutableState<List<HMSMessage>> = mutableStateOf(emptyList())
+    fun login(name: String) {
+        _loginState.value = LoginState.LOADING
+        disposable.add(
+            joinRoom(name)
+                .subscribeOn(Schedulers.io())
+                .subscribe(
+                    {},
+                    { e -> _loginState.value = LoginState.Error(e) }
+                )
+        )
+    }
 
-    fun login(name : String) {
-        loginRepository.login(name)
-            .map{ loginRepository.joinRoom(name, it.token, object : HMSUpdateListener{
+    private fun joinRoom(name: String): Observable<Unit> = loginRepository.login(name)
+        .doOnError {
+            Timber.e("Error getting token: ${it.message}")
+        }
+        .map {
+            Timber.d("A token response was received")
+            loginRepository.joinRoom(name, it.token, object : HMSUpdateListener {
                 override fun onError(error: HMSException) {
                     Timber.e("There was an error ${error.description}")
                     _loginState.value = LoginState.Error(error)
@@ -64,16 +83,18 @@ class RoomVm @Inject constructor(private val loginRepository: LoginRepository) :
                     // Somebody's audio/video changed
                     Timber.d("OnTrackUpdate: $type")
                 }
+            })
+        }
 
-            }) }
+    override fun onCleared() {
+        disposable.dispose()
+        super.onCleared()
     }
-
-
 }
 
 sealed class LoginState {
     object IDLE : LoginState()
     object LOADING : LoginState()
     object LoggedIn : LoginState()
-    data class Error(val exception : Exception) : LoginState()
+    data class Error(val exception: Throwable) : LoginState()
 }
