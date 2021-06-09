fd7546907130aeb52590b59dbf075d7210a1c3a7
Aniket Kadam <anikadamg@gmail.com>
2021-06-09 16:48:44 +0530

    Changed where the roomid is set

--- app/src/main/java/com/aniketkadam/videocon/joinroom/LoginRepository.kt
+++ app/src/main/java/com/aniketkadam/videocon/joinroom/LoginRepository.kt
@@ -3,6 +3,7 @@ package com.aniketkadam.videocon.joinroom
 import com.aniketkadam.videocon.tokenretriever.TokenRequest
 import com.aniketkadam.videocon.tokenretriever.TokenRequestApi
 import com.aniketkadam.videocon.tokenretriever.TokenResponse
+import com.google.gson.JsonObject
 import io.reactivex.rxjava3.core.Observable
 import live.hms.video.sdk.HMSSDK
 import live.hms.video.sdk.HMSUpdateListener
@@ -15,8 +16,11 @@ class LoginRepository @Inject constructor(
     private val hmssdk: HMSSDK
 ) {
 
-    fun login(userName: String): Observable<TokenResponse> {
-        return tokenRequestApi.getToken(TokenRequest(userId = userName))
+    fun login(
+        userName: String,
+        roomId: String = "60bcef7810772c81240d84d8"
+    ): Observable<TokenResponse> {
+        return tokenRequestApi.getToken(TokenRequest(userId = userName, roomId = roomId))
     }
 
     fun joinRoom(userName: String, authToken: String, updateListener : HMSUpdateListener) {
--- app/src/main/java/com/aniketkadam/videocon/tokenretriever/TokenRequest.kt
+++ app/src/main/java/com/aniketkadam/videocon/tokenretriever/TokenRequest.kt
@@ -3,7 +3,7 @@ package com.aniketkadam.videocon.tokenretriever
 import com.google.gson.annotations.SerializedName
 
 data class TokenRequest(
-    @SerializedName("room_id") val roomId : String = "60bcef7810772c81240d84d8",
+    @SerializedName("room_id") val roomId : String,
     @SerializedName("user_id") val userId : String,
     @SerializedName("role") val role : String = "Student"
 )
