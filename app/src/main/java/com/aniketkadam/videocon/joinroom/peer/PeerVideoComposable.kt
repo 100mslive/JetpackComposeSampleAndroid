c84432a9bc131f1b9beded5935e8816050c7df65
Aniket Kadam <anikadamg@gmail.com>
2021-06-13 12:57:18 +0530

    Fixed issues with the PeerVideo It was just that a shared context had to be used. Also appropriately releasing and acquiring the lock for the surface.

--- app/src/main/java/com/aniketkadam/videocon/joinroom/peer/PeerVideoComposable.kt
+++ app/src/main/java/com/aniketkadam/videocon/joinroom/peer/PeerVideoComposable.kt
@@ -1,23 +1,50 @@
 package com.aniketkadam.videocon.joinroom.peer
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.viewinterop.AndroidView
 import live.hms.video.sdk.models.HMSPeer
-import org.webrtc.EglBase
+import live.hms.video.utils.SharedEglContext
+import org.webrtc.RendererCommon
 import org.webrtc.SurfaceViewRenderer
+import timber.log.Timber
 
 @Composable
 fun PeerVideoComposable(peer: HMSPeer) {
 
+    var previousActivePeer by remember { mutableStateOf(peer) }
+    var previousVideoEnabled by remember { mutableStateOf(false) }
+
     AndroidView(
         factory = { context ->
             SurfaceViewRenderer(context).apply {
-                init(EglBase.create().eglBaseContext, null)
+                init(SharedEglContext.context, null)
+                setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT)
+                setEnableHardwareScaler(true)
             }
         },
         update = {
-            it.release() // Line 148 of egl renderer checks, so it's *always* safe to release without acquiring
-            peer.videoTrack?.addSink(it)
+            Timber.d("Previous: ${previousActivePeer.peerID}/${previousActivePeer.name}, new: ${peer.peerID}/${peer.name}")
+            // Peer changed, tile is rebound to a new peer.
+            if (previousActivePeer.peerID != peer.peerID) {
+                if (previousVideoEnabled) {
+                    Timber.d("Releasing video, removing sink")
+                    previousActivePeer.videoTrack?.removeSink(it)
+                    it.release()
+                } else {
+                    Timber.d("Not releasing video since it was never enabled")
+                }
+                previousActivePeer = peer
+            } else {
+                // Peer's the same or new, just re-rendered or ran the first time.
+                if (previousVideoEnabled && peer.videoTrack != null) {
+                    Timber.d("Peer ${peer.peerID} name:${peer.name} had no video")
+                } else if (!previousVideoEnabled && peer.videoTrack != null) {
+                    peer.videoTrack?.addSink(it)
+                    Timber.d("Peer ${peer.name} had video, adding")
+                }
+            }
+
+            previousVideoEnabled = peer.videoTrack == null
         }
     )
 
