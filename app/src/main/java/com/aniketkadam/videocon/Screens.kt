b25adf5378ce85bfe27d4089d3dff353f6260b8b
Aniket Kadam <aniket.kadam@100ms.live>
2022-05-31 12:16:24 +0530

    Added a leave button

--- app/src/main/java/com/aniketkadam/videocon/Screens.kt
+++ app/src/main/java/com/aniketkadam/videocon/Screens.kt
@@ -1,6 +1,9 @@
 package com.aniketkadam.videocon
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
@@ -14,8 +17,7 @@ import com.aniketkadam.videocon.loading.LoadingScreen
 import com.aniketkadam.videocon.login.LoginScreen
 import com.aniketkadam.videocon.login.LoginVm
 import com.aniketkadam.videocon.navigation.Screen
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 
 fun NavGraphBuilder.getLoginScreen(navController: NavHostController) {
@@ -41,14 +43,29 @@ fun NavGraphBuilder.getRoomScreen(
         if (roomVm.loadingState.value) {
             LoadingScreen()
         } else {
+            // Leave the room when the user swipes back.
             BackHandler {
                 roomVm.leave {
-                    CoroutineScope(Dispatchers.Main).launch {
+                    MainScope().launch {
                         navController.navigate(Screen.LOGIN.route)
                     }
                 }
             }
-            VideosListComposable(roomVm.peers)
+            Column {
+                // A leave button at the top
+                Button(onClick = {
+                    roomVm.leave {
+                        MainScope().launch {
+                            navController.navigate(Screen.LOGIN.route)
+                        }
+                    }
+                }) {
+                    Text("Leave")
+                }
+
+                // Peers and their video tiles
+                VideosListComposable(roomVm.peers)
+            }
         }
     }
 }
